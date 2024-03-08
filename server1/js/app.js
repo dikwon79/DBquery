39132020a30ee9cb83cb9082077adbcc21a74836
Jack Berena <jberenaa@gmail.com>
2024-03-08 02:15:58 -0800

    changed declarations

--- server1/js/app.js
+++ server1/js/app.js
@@ -1,5 +1,5 @@
 document.getElementById('insert').addEventListener('click', function() {
-    var data = JSON.stringify({
+    const data = JSON.stringify({
         query: "INSERT INTO patients (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')"
     });
     sendRequest('POST', 'http://localhost:8080/lab5/api/v1/sql', data);
@@ -7,17 +7,17 @@ document.getElementById('insert').addEventListener('click', function() {
 
 
 document.getElementById('submitQuery').addEventListener('click', function() {
-    var query = document.getElementById('sqlQuery').value.trim();
-    var isSelect = query.toLowerCase().startsWith('select');
-    var method = isSelect ? 'GET' : 'POST';
-    var url = isSelect ? `http://localhost:8080/lab5/api/v1/sql?query=${encodeURIComponent(query)}` : 'http://localhost:8080/lab5/api/v1/sql';
-    var data = isSelect ? null : JSON.stringify({ query: query });
+    const query = document.getElementById('sqlQuery').value.trim();
+    const isSelect = query.toLowerCase().startsWith('select');
+    const method = isSelect ? 'GET' : 'POST';
+    const url = isSelect ? `http://localhost:8080/lab5/api/v1/sql?query=${encodeURIComponent(query)}` : 'http://localhost:8080/lab5/api/v1/sql';
+    const data = isSelect ? null : JSON.stringify({ query: query });
 
     sendRequest(method, url, data);
 });
 
 function sendRequest(method, url, data) {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
